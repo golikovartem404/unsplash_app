002fe6ab26f9801417007131358e146cbdb8e448
golikovartyom <golikovartem404@gmail.com>
2023-01-02 17:34:15 +0400

    Initial Commit

---
+++ unsplash_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  unsplash_app
+//
+//  Created by User on 02.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
